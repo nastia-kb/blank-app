82d20824fa3a5dd2893f7a03a7f5414d462aa725
nastia-kb <aekobeleva@gmail.com>
2025-03-14 09:46:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,8 +76,6 @@ if st.button("Запустить"):
          for j in ans:
              data[j] = answers.str.contains(j)
            
-         data
-         ans
          temp =  data.groupby("0_concept_n").sum()[ans].T
          temp["Среднее по концептам"] = temp.sum(axis = 1)
          bases = list(data.groupby("0_concept_n").count()[ans[0]])
