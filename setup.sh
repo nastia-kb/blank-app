6e29f0685af9157420ceaecea525de9450fbbe40
nastia-kb <aekobeleva@gmail.com>
2025-04-25 13:20:06 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@ python -m venv venv
 source venv/bin/activate
 
 # Install base requirements
-pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt
+ACCEPT_EULA=Y  pip install --upgrade pip setuptools wheel
+ACCEPT_EULA=Y  pip install -r requirements.txt
 
 # Install spaCy from source
 git clone https://github.com/explosion/spaCy
 cd spaCy
-pip install -r requirements.txt
-pip install --no-build-isolation --editable .
+ACCEPT_EULA=Y  pip install -r requirements.txt
+ACCEPT_EULA=Y pip install --no-build-isolation --editable .
 
 # Download Russian model
 python -m spacy download ru_core_news_lg
